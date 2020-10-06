c210f532e4113524ef85c90f2faa42e464f92a90
Samuele <s.fusaro@geniodilignce.it>
2020-10-06 22:09:19 +0200

    sistemato codice e costanti

--- app.py
+++ app.py
@@ -4,16 +4,15 @@ import requests
 
 from queries import QUERIES
 
-def measure_performance(db, query):
-    iterations = 20
-    start = time.time()
-    # print(requests.get(url).text)
+ITERATIONS = 20
+
+def measure_performance(db, query):      
     if db == "virtuoso":
-        return sum([requests.get(query).elapsed.total_seconds() for x in range(iterations)]) / iterations
+        return sum([requests.get(query).elapsed.total_seconds() for x in range(ITERATIONS)]) / ITERATIONS
     if db == "graphdb":
         url="http://localhost:7200/repositories/KM4City"
         headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
-        return sum([requests.request("POST", url, headers=headers, data = query).elapsed.total_seconds()  for x in range(iterations)]) / iterations
+        return sum([requests.request("POST", url, headers=headers, data = query).elapsed.total_seconds()  for x in range(ITERATIONS)]) / ITERATIONS
 
 results = {
     "virtuoso": [],
